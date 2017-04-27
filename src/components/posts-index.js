a68b3456ecbcc5a3fa557bb4a140fc02bdb3c5cd
schesnowitz <steve@chesnowitz.com>
2017-04-27 01:12:07 -0400

    working on router component

---
+++ src/components/posts-index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+class PostsIndex extends React.Component { 
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>List o Posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
